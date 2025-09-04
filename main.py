6fbed0ae736e6e247162a70e36d0d9391716c5e8
genesisblock3301 <nur15-1463@diu.edu.bd>
2025-09-04 14:34:20 +0600

    initial commit

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
 
 model = OllamaLLM(model="llama3.2")
 
 template = """
-You are an exper in answering questions about a pizza restaurant.
-Here are some relevant reviews:  {reviews}
+You are an expert in answering questions about a pizza restaurant
+
+Here are some relevant reviews: {reviews}
+
 Here is the question to answer: {question}
 """
-
 prompt = ChatPromptTemplate.from_template(template)
-
 chain = prompt | model
 
-
 while True:
-
-    question = input("What would you like to answer? q for quite\n")
+    print("\n\n-------------------------------")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n")
     if question == "q":
         break
-    result = chain.invoke({
-        "reviews": [],
-        "question": "What is the best pizza place in town?",
-    })
 
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews": reviews, "question": question})
     print(result)
